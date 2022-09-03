559001632d6767f714603070c2da9288e8f9b607
Clayton Maciel <62034289+claytonmaciel@users.noreply.github.com>
2022-09-03 11:39:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ eachLine('./PI.txt', (line, last) => {
 
     const numPI = line
 
-    function ePrimo(num) {
+    function vPrimo(num) {
         for(let i = 2, s = Math.sqrt(num); i <= s; i++)
             if(num % i === 0) return false
         return true
@@ -16,46 +16,25 @@ eachLine('./PI.txt', (line, last) => {
 
     for (let i = 0; i < numPI.length-9; i++) {
 
-        if (numPI[i] !== numPI[i+8])
+        if (numPI[i] !== numPI[i+8] || numPI[i+8] % 2 == 0) //se o número primeiro dígito for diferente do último dígito ou se o último dígito for par, continue.
         continue
             
-        let num = ""
+        let num = "", j = 0, numr = ""
 
-        let j = 0
-        while (numPI[i+j] === numPI[8+i-j] && j < 9){
+        while (numPI[i+j] === numPI[8+i-j] && j < 4){ //verifica se os 4 primeiros dígitos são iguais aos 4 últimos. 
             num += numPI[i+j]
+            numr = numPI[i+j] + numr
             j++
         }
-            
-        if (j === 9 && numPI[i+j-1] % 2 !== 0){
-            //verifica primo
-            //se primo, imprime e break
-            if (ePrimo(num)){
-                console.log(num)
-
-                const fim = Date.now()
-                console.log(`Tempo de execução: ${fim - inicio} milisegundos`)
-                break
-                
-            }
+        
+        if (j === 4 && vPrimo(num=num+numPI[i+j]+numr)){
+            console.log(num)
+            const fim = Date.now()
+            console.log(`Tempo de execução: ${fim - inicio} milisegundos`)
+            break
         }
     }
-
     if (last) {
         return false; // stop reading
     }
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-    
