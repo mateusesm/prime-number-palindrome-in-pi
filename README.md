b6c3bdd0883604a2134f9de09aac38e94c11a94e
Mateus Macedo <mateusemanuel198@gmail.com>
2022-09-03 13:11:29 -0300

    update in the settings from project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Algoritmo que lê expansão decimal do número PI (3,1415...) e traz o primeiro número palíndromo primo de 9 dígitos
 
-O algoritmo foi criado em JavaScript utilizando Node JS e um módulo chamado Line Reader para ler arquivos de texto. Neste projeto, disponibilizamos um arquivo de texto contendo a expansão decimal de 1 milhão de dígitos do número PI para leitura e testes, lembrando que pode-se usar qualquer número de expansão decimal inferior a 1 milhão e que seja maior do que 9 ou superior a 1 milhão de dígitos, se preferir, atentando-se sempre para possíveis mudanças em nomes de arquivos, diretórios e extensões para que o método eachLine() do Line Reader consiga ler o arquivo com sucesso.
+O algoritmo foi criado em JavaScript utilizando Node JS. Neste projeto, disponibilizamos um arquivo de texto contendo a expansão decimal de 1 milhão de dígitos do número PI para leitura e testes, lembrando que pode-se usar qualquer número de expansão decimal inferior a 1 milhão e que seja maior do que 9 ou superior a 1 milhão de dígitos, se preferir, atentando-se sempre para possíveis mudanças em nomes de arquivos, diretórios e extensões.
 
 Abaixo está um pequeno guia de instalação para rodar o projeto.
 
@@ -13,14 +13,6 @@ node --version
 ```
 
 O comando deverá retornar a versão do Node JS instalada. Caso não retorne, baixe-a.
-
-Em seguida, use o gerenciador de pacotes de sua preferência (npm, yarn, etc.) para instalar todas as dependências
-
-No meu caso, usei o npm
-
-```bash
-npm install
-```
 
 ### Rodando o projeto
 
--- index.js
+++ index.js
@@ -1,42 +1,37 @@
-const lineReader = require('line-reader')
-
-const { eachLine } = lineReader
-
 const inicio = Date.now()
 
-eachLine('./PI.txt', (line, last) => {
+const PI = require('./PI.js')
 
-    const numPI = line
+const numPI = PI.variable
 
-    function vPrimo(num) {
-        for(let i = 2, s = Math.sqrt(num); i <= s; i++)
-            if(num % i === 0) return false
-        return true
-    }
+function vPrimo(num) {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false
+    return true
+}
 
-    for (let i = 0; i < numPI.length-9; i++) {
+for (let i = 2; i < numPI.length-9; i++) {
 
-        if (numPI[i] !== numPI[i+8] || numPI[i+8] % 2 == 0) //se o número primeiro dígito for diferente do último dígito ou se o último dígito for par, continue.
-        continue
+    if (numPI[i] !== numPI[i+8] || numPI[i+8] % 2 == 0) //se o número primeiro dígito for diferente do último dígito ou se o último dígito for par, continue.
+    continue
             
-        let num = "", j = 0, numr = ""
+    let num = "", j = 0, numr = ""
 
-        while (numPI[i+j] === numPI[8+i-j] && j < 4){ //verifica se os 4 primeiros dígitos são iguais aos 4 últimos. 
-            num += numPI[i+j]
-            numr = numPI[i+j] + numr
-            j++
-        }
+    while (numPI[i+j] === numPI[8+i-j] && j < 4){ //verifica se os 4 primeiros dígitos são iguais aos 4 últimos. 
+        num += numPI[i+j]
+        numr = numPI[i+j] + numr
+        j++
+    }
         
-        if (j === 4 && vPrimo(num=num+numPI[i+j]+numr)){
-            console.log(num)
-            const fim = Date.now()
-            console.log(`Tempo de execução: ${fim - inicio} milisegundos`)
-            const used = process.memoryUsage().heapUsed / 1024 / 1024;
-            console.log(`O script usou aproximadamente ${Math.round(used * 100) / 100} MB`);
-            break
-        }
+    if (j === 4 && vPrimo(num=num+numPI[i+j]+numr)){
+        console.log(num)
+        break
     }
-    if (last) {
-        return false; // stop reading
-    }
-});
+}
+  
+const fim = Date.now()
+console.log(`Tempo de execução: ${fim - inicio} milisegundos`)
+
+const used = process.memoryUsage().heapUsed / 1024 / 1024
+console.log(`O script usou aproximadamente ${Math.round(used * 100) / 100} MB`)
+
