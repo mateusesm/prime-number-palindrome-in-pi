e30844883f3a845556682c08d6bcd4023b657770
Mateus Macedo <mateusemanuel198@gmail.com>
2022-09-06 10:47:10 -0300

    update index.js with comments

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const inicio = Date.now()
+const inicio = Date.now() // Pega o tempo em milisegundos antes do algoritmo executar
 
-//import PI from "./PI.js"
+//import PI from "./PI.js" // Chama um arquivo JS exportando a string do número PI com 1 milhão de casas decimais
 
-//const numPI = PI
+//const numPI = PI // Coloca a string do número PI contendo 1 milhão de casas decimais na variável numPI
 
-import fetch from "node-fetch";
+import fetch from "node-fetch"; // Importa o módulo node-fetch para fazer fetch e consumir APIS
 
-function getPI(start) {
+function getPI(start) { // Função que recebe o número em que queremos iniciar a busca pelas casas decimais do PI e retorna o fetch
     return fetch(`https://api.pi.delivery/v1/pi?start=${start}&numberOfDigits=1000&radix=10`)
 }
 
-function vPrimo(num) {
+function vPrimo(num) { // Função que verifica se o número é primo
     for(let i = 2, s = Math.sqrt(num); i <= s; i++)
         if(num % i === 0) return false
     return true
 }
 
-function calcPrimePalindrome(numPI) {
+function calcPalindromeNumber(numPI) { // Função que recebe o intervalo de casa decimais do PI e verifica entre elas se existe um palindromo para mandar para a função vPrimo verificar se é primo
     for (let i = 0; i < numPI.length-21; i++) {
         
-        if (numPI[i] !== numPI[i+20] || numPI[i+20] % 2 == 0) //se o número primeiro dígito for diferente do último dígito ou se o último dígito for par, continue.
+        if (numPI[i] !== numPI[i+20] || numPI[i+20] % 2 == 0) // Se o número primeiro dígito for diferente do último dígito ou se o último dígito for par, continue
         continue
                 
         let num = "", j = 0, numr = ""
     
-        while (numPI[i+j] === numPI[20+i-j] && j < 10){ //verifica se os 4 primeiros dígitos são iguais aos 4 últimos. 
+        while (numPI[i+j] === numPI[20+i-j] && j < 10){ // Verifica se os 10 primeiros dígitos são iguais aos 10 últimos. 
             num += numPI[i+j]
             numr = numPI[i+j] + numr
             j++
         }
             
-        if (j === 10 && vPrimo(num=num+numPI[i+j]+numr)){
+        if (j === 10 && vPrimo(num=num+numPI[i+j]+numr)){ // Caso o número seja palindromo e primo, retorna o número, caso contrário, retorna undefined
             return num
         }
     }
@@ -38,31 +38,30 @@ function calcPrimePalindrome(numPI) {
     return
 }
 
-let start = 0
-let end = 100000
+let start = 0 // Variável de controle que controla por qual casa decimal de PI iremos começar a busca
+let end = 100000 // Variável de controle do loop que controla quantas vezes irá se repetir
 
-async function palindromePI() {
-    for (let c = 0; c <= end; c++) {
+async function palindromePI() { // Função assíncrona que executa a função que faz requisição para a API e recebe uma promise para tratá-la e obter o intervalo de casas decimais de PI desejadas
+    for (let c = 0; c <= end; c++) { // Loop que fica fazendo requisições a API
 
-        const response = await getPI(start, 1000)
-        const responseJson = await response.json()
+        const response = await getPI(start, 1000) // A variável response recebe uma promise que foi resolvida com o await e trazida pela função getPI mandando o número de início e a quantidade máxima de dígitos retornáveis
+        const responseJson = await response.json() // A variável responseJson recebe o resultado dessa promise convertida para JSON
 
-        const numPalindromePI = calcPrimePalindrome(responseJson.content)
-
-        if (numPalindromePI) {
-            console.log(numPalindromePI)
+        if (calcPalindromeNumber(responseJson.content)) { // Manda o intervalo de 1000 dígitos de PI para verificar
+            const numPalindromePI = calcPalindromeNumber(responseJson.content) // Se retornar um número, é recebido pela variável
+            console.log(numPalindromePI) // Em seguida, mostrado na tela e para a execução
             break
         }
 
-        start = start + 1000 - 9
+        start = start + 1000 - 21 // A cada iteração a variável de start recebe ela mesma mais 1000 - 9 para chamar um novo intervalo de casas decimais e subtrair 21 para evitar que os 21 últimos formem pares palindromos com a próxima "remessa"
     
-        if (start >= 100000000) break
+        if (start >= 100000000) break // Para ter um pouco mais de controle sobre a execução, ao chegar em 100 milhões o programa para a execução
     }
 
-    const fim = Date.now()
+    const fim = Date.now() // Pega o tempo em milisegundos depois do algoritmo executar para mostrar o tempo decorrido
     console.log(`Tempo de execução: ${fim - inicio} milisegundos`)
     
-    const used = process.memoryUsage().heapUsed / 1024 / 1024
+    const used = process.memoryUsage().heapUsed / 1024 / 1024 // Pega o processamento para mostrar a memória usada na execução do algoritmo
     console.log(`O script usou aproximadamente ${Math.round(used * 100) / 100} MB`)
   
 }
